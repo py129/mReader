6b7a77785f51cbc0abf5b0bb5059eaedfffbe24a
pengyang <pengyang@huitongjy.com>
2018-10-15 21:26:01 +0800

    first

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+var request = require('../tool/request.js');
+
 /* GET home page. */
 router.get('*', function(req, res, next) {
   res.render('index');
 });
+
+router.post('/*',function(req,res) {
+    req.setTimeout(60000);
+    if (req.originalUrl.indexOf('zhuishushenqi') > -1){
+         let url = req.originalUrl.replace(/\/zhuishushenqi/,'')
+         url = 'http://api.zhuishushenqi.com' + url
+         let obj={
+             ...req.body
+         }
+         request.get(url, obj, function(data){
+             res.json(data);
+         },req,res)
+     } else {
+         request.post(req.path,req.body,function(data){
+             res.json(data);
+         },req,res)
+     }
+ })
 
 module.exports = router;
