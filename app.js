8758b8d75ed4583235003de5357c7d50dedcd5c7
pengyang <pengyang@huitongjy.com>
2018-10-16 20:47:24 +0800

    first

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use("/",express.static(path.join(__dirname, 'dist')));
 app.use("/apis",express.static(path.join(__dirname, 'public')));
 app.use('*', index);
