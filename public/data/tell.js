4d267aa57902b768d1330dc0c5ca240eaa25761b
pengyang <pengyang@huitongjy.com>
2018-11-09 15:27:22 +0800

    时间戳

--- public/data/tell.js
+++ public/data/tell.js
@@ -1,15 +1,5 @@
-
-if (ssage){
-    ssage({
-        start: 1541745600000,
-        end: 1541746500000,
-        msg: '<div>这是一段公告<span style="color: #f00">奥德赛佛阿萨德</span>佛收到货佛山的佛哈手动阀哈说得好手动发货事发后的后代送</div>'
-    })
-}
-if (message){
-    message({
-        start: 1541745600000,
-        end: 1541746500000,
-        msg: '<div>这是一段公告<span style="color: #f00">奥德赛佛阿萨德</span>佛收到货佛山的佛哈手动阀哈说得好手动发货事发后的后代送</div>'
-    })
-}
+message({
+    start: 1541748600000,
+    end: 1541749140000,
+    msg: '<div>这是一段公告<span style="color: #f00">奥德赛佛阿萨德</span>佛收到货佛山的佛哈手动阀哈说得好手动发货事发后的后代送</div>'
+})
