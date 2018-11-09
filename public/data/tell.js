16388f8a5cbf926abee248e5efa393f6c3e54919
pengyang <pengyang@huitongjy.com>
2018-11-09 14:29:11 +0800

    editor

--- public/data/tell.js
+++ public/data/tell.js
@@ -1,8 +1,5 @@
 message({
-    time: 1234,
-    name: '哈哈哈哈',
-    data: {
-        age: 13,
-        title: '呱呱呱呱'
-    }
+    start: 1541745000000,
+    end: 1541745060000,
+    msg: '<div>这是一段公告<span style="color: #f00">奥德赛佛阿萨德</span>佛收到货佛山的佛哈手动阀哈说得好手动发货事发后的后代送</div>'
 })
