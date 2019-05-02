ca47f0f3d4aaa271ced820aaa9c2bc4fca145642
pengyang <pengyang@huitongjy.com>
2019-05-02 17:55:38 +0800

    追书神器request

--- routes/zhuishuApi.js
+++ routes/zhuishuApi.js
@@ -6,24 +6,18 @@ var apis = require('../tool/apis.js');
 var util = require('../tool/util.js');
 
 router.get('*', function(req, res, next) {
-    console.log('=======1111============================')
-    console.log(req.query)
     let url = req.params['0'].split('/')[1]
     if (url){
         let obj={
             ...req.query
         }
         let postUrl = apis[url] || ''
-        console.log('============22222=======================')
-        console.log(postUrl)
         if(obj.paramsList){
             let arr = JSON.parse(obj.paramsList) || []
             arr.forEach(element => {
                 postUrl = postUrl + '/' + element
             });
         }
-        console.log('============33333=======================')
-        console.log( postUrl )
         request.get(postUrl, obj, function(data){
             res.json(data);
         },req,res)
